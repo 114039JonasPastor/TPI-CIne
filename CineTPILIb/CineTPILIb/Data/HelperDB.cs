077c950a1a73073a7bcb51dac37093a84a0d78b1
JonasPastor <114039@tecnicatura.frc.utn.edu.ar>
2023-11-03 00:32:06 -0300

    3/11

---
+++ CineTPILIb/CineTPILIb/Data/HelperDB.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineTPILIb.Data
+{
+    public class HelperDB
+    {
+        private static HelperDB instancia = null;
+        private SqlConnection conexion;
+
+        private HelperDB()
+        {
+            conexion = new SqlConnection("Data Source=DESKTOP-35MIDJI\\SQLEXPRESS;Initial Catalog=CINE;Integrated Security=True");
+        }
+        public static HelperDB ObtenerInstancia()
+        {
+            if(instancia == null)
+            {
+                instancia = new HelperDB();
+            }
+            return instancia;
+        }
+        public SqlConnection ObtenerConexion()
+        {
+            return this.conexion;
+        }
+        public DataTable Consultar(string NombreSp)
+        {
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NombreSp;
+
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+
+            conexion.Close();
+            return tabla;
+        }
+
+        public DataTable ConsultarConParametros(string NombreSP, List<Parametro> parametros)
+        {
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NombreSP;
+
+            comando.Parameters.Clear();
+            foreach (Parametro p in parametros)
+            {
+                comando.Parameters.Add(p.Nombre, (SqlDbType)p.Valor);
+            }
+
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+
+            conexion.Close();
+            return tabla;
+        }
+        public int ConsultarEscalar(string NombreSP, string NombreParametroOut)
+        {
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = NombreSP;
+
+            SqlParameter parametro = new SqlParameter();
+            parametro.ParameterName = NombreParametroOut;
+            parametro.SqlDbType = SqlDbType.Int;
+            parametro.Direction = ParameterDirection.Output;
+
+            comando.Parameters.Add(parametro);
+            comando.ExecuteNonQuery();
+
+            conexion.Close();
+
+            return (int)parametro.Value;
+        }
+    }
+}
---
+++ CineTPILIb/CineTPILIb/Dominio/DetallesTicket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineTPILIb.Dominio
+{
+    public class DetallesTicket
+    {
+        public int Id_detalle { get; set; }
+        public Funcion Funcion { get; set; }
+        public int Id_butaca { get; set; }
+        public double Precio_venta { get; set; }
+
+        public DetallesTicket(int id_detalle, Funcion funcion, int id_butaca, double precio_venta)
+        {
+            Id_detalle = id_detalle;
+            Funcion = funcion;
+            Id_butaca = id_butaca;
+            Precio_venta = precio_venta;
+        }
+    }
+}
